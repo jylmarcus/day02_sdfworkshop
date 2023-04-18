019fc72e9fd1e824982dad1d7faa7f5ba958d3e2
jylmarcus <maplecow@gmail.com>
2023-04-18 14:55:54 +0800

    Added members, constructors, getters, setters, methods

--- src/main/java/sg/edu/nus/iss/BankAccount.java
+++ src/main/java/sg/edu/nus/iss/BankAccount.java
@@ -13,6 +13,17 @@ public class BankAccount {
     private String accountCreated;
     private String accountClosed;
 
+    //constructors
+    public BankAccount(String accountHolder){
+        this.accountHolder = accountHolder;
+        setAccountBalance((float)0);
+    }
+
+    public BankAccount(String accountHolder, Float accountBalance){
+        this.accountHolder = accountHolder;
+        setAccountBalance((float)accountBalance);
+    }
+
     //getters
     public String getAccountHolder() {
         return accountHolder;
@@ -37,6 +48,7 @@ public class BankAccount {
     }
 
     //setters
+
     public void setAccountBalance(Float accountBalance) {
         this.accountBalance = accountBalance;
     }
@@ -53,4 +65,37 @@ public class BankAccount {
         this.accountClosed = accountClosed;
     }
     
+    //randomly generate a string of 10 numbers for account number
+    private String randomAccountNumber(){
+        Integer[] randomNumbers = new Integer[10];
+        Integer max = 9;
+        Integer min = 0;
+        Integer range = max-min + 1;
+
+        for(Integer i=0; i<randomNumbers.length; i++){
+            randomNumbers[i] = (int)(Math.random()*range) + min;
+        }
+        return randomNumbers.toString();
+    }
+
+    //methods
+    public void deposit(Float depositAmount) throws Exception{
+        if(depositAmount > 0) {
+            this.accountBalance += depositAmount;
+            String transaction = ("deposit $" + depositAmount + " at <date time>");
+            this.transactions.add(transaction);
+        } else {
+            throw new Exception("IllegalArgumentException");
+        }
+    }
+
+    public void withdraw(Float withdrawAmount) throws Exception{
+        if(withdrawAmount <= 0 || withdrawAmount > this.getAccountBalance()){
+            throw new Exception("IllegalArgumentException");
+        } else {
+            this.accountBalance -= withdrawAmount;
+            String transaction = ("withdraw $" + withdrawAmount + "at <date time>");
+            this.transactions.add(transaction);
+        }
+    }
 }
