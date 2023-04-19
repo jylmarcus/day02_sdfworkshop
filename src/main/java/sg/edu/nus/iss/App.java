d34a68754ccd8c3a5904511d306a8311f72372de
jylmarcus <maplecow@gmail.com>
2023-04-19 09:15:12 +0800

    Updated BankAccount and added tests

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -4,6 +4,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //first test
+        BankAccount bAccount1 = new BankAccount("Horation Hornblower", "0001");
+        System.out.println(bAccount1.toString());
+
+        //test methods
+        bAccount1.deposit(100);
+        System.out.println(bAccount1.toString());
+
+        bAccount1.withdraw(100);
+        System.out.println(bAccount1.toString());
+
+        bAccount1.deposit(-100); //negative deposit
+
+        bAccount1.withdraw(-100); //negative withdrawal
+
+        bAccount1.withdraw(100f); //over account balance
+
+        bAccount1.setIsClosed(true);
+
+        bAccount1.deposit(100); //deposit into closed account
+
+        bAccount1.withdraw(100); //withdraw from closed account
+
+        for(String transDetails: bAccount1.getTransactions()){
+            System.out.println(transDetails);
+        }
+
+
     }
 }
--- src/main/java/sg/edu/nus/iss/BankAccount.java
+++ src/main/java/sg/edu/nus/iss/BankAccount.java
@@ -8,11 +8,11 @@ public class BankAccount {
     //members
     private final String accountHolder;
     private String accountNumber;
-    private Float accountBalance;
+    private float accountBalance;
     private ArrayList<String> transactions;
-    private Boolean isClosed;
-    private String accountCreated;
-    private String accountClosed;
+    private boolean isClosed;
+    private Date accountCreated;
+    private Date accountClosed;
 
     //constructors
     public BankAccount(String accountHolder, String accountNumber){
@@ -20,14 +20,16 @@ public class BankAccount {
         this.accountNumber = accountNumber;
         this.accountBalance = 0.0f;
 
+        this.accountCreated = new Date();
         transactions = new ArrayList<>();
     }
 
-    public BankAccount(String accountHolder, String accountNumber, Float accountBalance){
+    public BankAccount(String accountHolder, String accountNumber, float accountBalance){
         this.accountHolder = accountHolder;
         this.accountNumber = accountNumber;
         this.accountBalance = accountBalance;
 
+        this.accountCreated = new Date();
         transactions = new ArrayList<>();
     }
 
@@ -38,37 +40,39 @@ public class BankAccount {
     public String getAccountNumber() {
         return accountNumber;
     }
-    public Float getAccountBalance() {
+    public float getAccountBalance() {
         return accountBalance;
     }
     public ArrayList<String> getTransactions() {
         return transactions;
     }
-    public Boolean getIsClosed() {
+    public boolean getIsClosed() {
         return isClosed;
     }
-    public String getAccountCreated() {
+    public Date getAccountCreated() {
         return accountCreated;
     }
-    public String getAccountClosed() {
+    public Date getAccountClosed() {
         return accountClosed;
     }
 
     //setters
 
-    public void setAccountBalance(Float accountBalance) {
+    public void setAccountBalance(float accountBalance) {
         this.accountBalance = accountBalance;
     }
     public void setTransactions(ArrayList<String> transactions) {
         this.transactions = transactions;
     }
-    public void setIsClosed(Boolean isClosed) {
+    public void setIsClosed(boolean isClosed) {
         this.isClosed = isClosed;
+
+        setAccountClosed(new Date());
     }
-    public void setAccountCreated(String accountCreated) {
+    public void setAccountCreated(Date accountCreated) {
         this.accountCreated = accountCreated;
     }
-    public void setAccountClosed(String accountClosed) {
+    public void setAccountClosed(Date accountClosed) {
         this.accountClosed = accountClosed;
     }
     
@@ -79,14 +83,21 @@ public class BankAccount {
         Integer min = 0;
         Integer range = max-min + 1;
 
-        for(Integer i=0; i<randomNumbers.length; i++){
+        for(int i=0; i<randomNumbers.length; i++){
             randomNumbers[i] = (int)(Math.random()*range) + min;
         }
         return randomNumbers.toString();
     }
 
     //methods
-    public void deposit(Float depositAmount){
+    @Override
+    public String toString() {
+        return "BankAccount [fullName=" + getAccountHolder() + ", accountNumber=" + getAccountNumber() + ", balance=" + getAccountBalance()
+                + ", isClosed=" + isClosed + ", accountStartDate=" + getAccountCreated() + ", accountEndDate="
+                + getAccountClosed() + "]";
+    }
+
+    public void deposit(float depositAmount){
         if(isClosed) {
             throw new IllegalArgumentException("Account has been closed. You cannot make a deposit to an inactive or closed account.");
         } else if(depositAmount <= 0) {
@@ -99,7 +110,7 @@ public class BankAccount {
         }
     }
 
-    public void withdraw(Float withdrawAmount){
+    public void withdraw(float withdrawAmount){
         if(withdrawAmount <= 0){
             throw new IllegalArgumentException("You cannot withdraw a negative amount");
         } else if(withdrawAmount > this.getAccountBalance()){
